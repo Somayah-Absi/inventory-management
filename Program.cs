cf77a4c64b13cb1d960df811619a4bcea2fbc917
Somayah-Absi <somayahahmedabsi@gmail.com>
2024-03-25 03:37:57 +0300

    first classItem

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.CompilerServices;
+class Item
+{
+
+
+    private string Name { get; }
+    private int Quantity { set; get; }
+    private DateTime CreateTime { set; get; }
+
+    public Item(string name, int quantity, DateTime createTime = default)
+    {
+        if (quantity > 0)
+        {
+            Quantity = quantity;
+        }
+        else { Console.WriteLine("the amount cannot be negative"); }
+        Name = name;
+
+        CreateTime = createTime == default ? DateTime.Now : createTime;
+
+    }
+
+    public override string ToString()
+    {
+        return $"Item Name:{Name} \n Quantity: {Quantity} \n Date: {CreateTime}";
+    }
+
+
+}
+public class MyProgram
+{
+
+    public static void Main(string[] args)
+    {
+
+    }
+}
