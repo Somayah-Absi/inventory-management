126b03d6a2949630d72805d0f36d66cd75c3b6cd
Somayah-Absi <somayahahmedabsi@gmail.com>
2024-03-26 01:47:36 +0300

    finish level 2

--- Program.cs
+++ Program.cs
@@ -34,8 +34,21 @@ class Item
 class Store
 {
     private List<Item> items = new List<Item>();
+    private int maxCapacity;
+
+    public Store(int capacity)
+    {
+        maxCapacity = capacity;
+    }
+
+
     public void AddItem(Item item)
     {
+        if (items.Count >= maxCapacity)
+        {
+            Console.WriteLine("Cannot add item. Store is at maximum capacity.");
+            return;
+        }
         bool isExist = items.Any(check => check.NameGet == item.NameGet);
         if (isExist)
         {
@@ -67,6 +80,7 @@ class Store
         }
         else
         {
+
             Console.WriteLine("There is no item with the given name");
         }
     }
@@ -94,38 +108,51 @@ public class MyProgram
 {
     public static void Main(string[] args)
     {
-        Store store = new Store();
 
-        // Add items to the store
-        store.AddItem(new Item("Water Bottle", 10, new DateTime(2023, 1, 1)));
-        store.AddItem(new Item("Chocolate Bar", 15, new DateTime(2023, 2, 1)));
-        store.AddItem(new Item("Notebook", 5, new DateTime(2023, 3, 1)));
-        store.AddItem(new Item("Pen", 20, new DateTime(2023, 4, 1)));
-        store.AddItem(new Item("Tissue Pack", 30, new DateTime(2023, 5, 1)));
-        store.AddItem(new Item("Chips Bag", 25, new DateTime(2023, 6, 1)));
-        store.AddItem(new Item("Soda Can", 8, new DateTime(2023, 7, 1)));
-        store.AddItem(new Item("Soap", 12, new DateTime(2023, 8, 1)));
-        store.AddItem(new Item("Shampoo", 40, new DateTime(2023, 9, 1)));
-        store.AddItem(new Item("Toothbrush", 50, new DateTime(2023, 10, 1)));
-        store.AddItem(new Item("Coffee", 20));
-        store.AddItem(new Item("Sandwich", 15));
-        store.AddItem(new Item("Batteries", 10));
-        store.AddItem(new Item("Umbrella", 5));
-        store.AddItem(new Item("Sunscreen", 8));
 
-        // Example usage of store methods
+        // items example - You do not need to follow exactly the same
+        var waterBottle = new Item("Water Bottle", 10, new DateTime(2023, 1, 1));
+        var chocolateBar = new Item("Chocolate Bar", 15, new DateTime(2023, 2, 1));
+        var notebook = new Item("Notebook", 5, new DateTime(2023, 3, 1));
+        var pen = new Item("Pen", 20, new DateTime(2023, 4, 1));
+        var tissuePack = new Item("Tissue Pack", 30, new DateTime(2023, 5, 1));
+        var chipsBag = new Item("Chips Bag", 25, new DateTime(2023, 6, 1));
+        var sodaCan = new Item("Soda Can", 8, new DateTime(2023, 7, 1));
+        var soap = new Item("Soap", 12, new DateTime(2023, 8, 1));
+        var shampoo = new Item("Shampoo", 40, new DateTime(2023, 9, 1));
+        var toothbrush = new Item("Toothbrush", 50, new DateTime(2023, 10, 1));
+        var coffee = new Item("Coffee", 20);
+        var sandwich = new Item("Sandwich", 15);
+        var batteries = new Item("Batteries", 10);
+        var umbrella = new Item("Umbrella", 5);
+        var sunscreen = new Item("Sunscreen", 8);
+
+        var store = new Store(10);
+
+        store.AddItem(waterBottle);
+        store.AddItem(chocolateBar);
+        store.AddItem(notebook);
+        store.AddItem(pen);
+        store.AddItem(tissuePack);
+        store.AddItem(chipsBag);
+        store.AddItem(sodaCan);
+        store.AddItem(soap);
+        store.AddItem(shampoo);
+        store.AddItem(toothbrush);
+        store.AddItem(coffee);
+        store.AddItem(sandwich);
+        store.AddItem(batteries);
+
         Console.WriteLine($"Current volume: {store.GetCurrentVolume()}");
 
-        Item foundItem = store.FindItemByName("Chocolate Bar");
-        if (foundItem != null)
-        {
-            Console.WriteLine(foundItem);
-        }
+        store.FindItemByName("Chocolate Bar");
+        store.FindItemByName("Batteries");
 
         store.DeleteItem("Chocolate Bar");
         Console.WriteLine($"Current volume after deletion: {store.GetCurrentVolume()}");
 
         List<Item> sortedItems = store.SortByNameAsc();
+        Console.WriteLine("_________________________________________________");
         foreach (var item in sortedItems)
         {
             Console.WriteLine(item);
